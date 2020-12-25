4507efad74bcc442154cb444c8a6a6505358b670
vokhmianin-da <ishchar@mail.ru>
2020-12-25 11:05:58 +0300

    Форма 1

---
+++ QtLanguage_en.ts
@@ -0,0 +1,96 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>Text Editor</source>
+        <translation>Text Editor</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="66"/>
+        <source>Сохранить</source>
+        <translation>Save</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="83"/>
+        <location filename="mainwindow.cpp" line="60"/>
+        <source>Открыть</source>
+        <translation>Open</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="59"/>
+        <location filename="mainwindow.cpp" line="40"/>
+        <source>Сохранить как</source>
+        <translation>Save as</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="73"/>
+        <location filename="mainwindow.cpp" line="100"/>
+        <source>Справка</source>
+        <translation>Help</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="36"/>
+        <source>Только для чтения</source>
+        <translation>Only read</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="52"/>
+        <source>Создать</source>
+        <translation>Create</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="130"/>
+        <source>Язык:</source>
+        <translation>Language:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="180"/>
+        <source>Назначить клавишу &quot;Открыть&quot;</source>
+        <translation>Assign key &quot;Open&quot;</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="201"/>
+        <source>Назначить клавишу &quot;Сохранить&quot;</source>
+        <translation>Assign key &quot;Save&quot;</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="225"/>
+        <source>Тема:</source>
+        <translation>Application:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="194"/>
+        <source>Назначить клавишу &quot;Создать&quot;</source>
+        <translation>Assign key &quot;Create&quot;</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="187"/>
+        <source>Назначить клавишу &quot;Выход&quot;</source>
+        <translation>Assign key &quot;Quit&quot;</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="17"/>
+        <source>Текстовый файл(*.txt)</source>
+        <translation>Text file(*.txt)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="250"/>
+        <source>Светлая</source>
+        <translation>Light</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="238"/>
+        <location filename="mainwindow.ui" line="245"/>
+        <source>Темная</source>
+        <translation>Dark</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="277"/>
+        <source>Создать документ</source>
+        <translation>Create document</translation>
+    </message>
+</context>
+</TS>
--- TextEditor_v2.pro
+++ TextEditor_v2.pro
@@ -18,3 +18,9 @@ SOURCES += main.cpp\
 HEADERS  += mainwindow.h
 
 FORMS    += mainwindow.ui
+
+RESOURCES += \
+    res.qrc
+
+TRANSLATIONS += QtLanguage_en.ts
+CODECFORSRC     = UTF-8
