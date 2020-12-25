8c1fbedbc269df7c59c6ffcfbcd218ba4aac3f0c
vokhmianin-da <ishchar@mail.ru>
2020-12-25 11:34:10 +0300

    Есть выбор темы

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,9 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFile>
+#include <QFileDialog>
+#include <QTextStream>
+#include <QLocale>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +15,27 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_actWhite_triggered()    //выбор белой темы
+{
+    application = "";
+    qApp->setStyleSheet(application);
+    ui->actWhite->setChecked(true);
+    ui->actDark->setChecked(false);
+}
+
+void MainWindow::on_actDark_triggered()     //выбор темной темы
+{
+    QFile file1(":/new/steels/style.qss");
+    if (file1.open(QFile::ReadOnly))
+    {
+        QTextStream stream(&file1);
+        stream.setCodec("UTF-8");    //ДЛЯ ОТОБРАЖЕНИЯ РУССКИХ БУКВ
+        application = stream.readAll();
+        file1.close();
+        qApp->setStyleSheet(application);
+        ui->actWhite->setChecked(false);
+        ui->actDark->setChecked(true);
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtWidgets>
+#include <QTranslator>
+#include <QEvent>
 
 namespace Ui {
 class MainWindow;
@@ -11,12 +14,45 @@ class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
+private:
+    Ui::MainWindow *ui;
+    QString filter;  //фильтр форматов файлов
+    QTextEdit* help; //для создания окна справки
+    QTranslator translator; //для переключения переводов
+    QString currentFileName = 0;    //хранит текущее имя файла
+    QString application = 0;    //хранит текущее имя файла
+
+    /*Для хранения текущих горячих клавиш*/
+    int saveKey = Qt::Key_S;
+    int openKey = Qt::Key_O;
+    int quitKey = Qt::Key_Q;
+    int newFileKey = Qt::Key_N;
+    unsigned int typeModifierSave = Qt::ControlModifier; //наличие и тип модификатора (CNTRL, SHIFT, ALT)
+    unsigned int typeModifierOpen = Qt::ControlModifier; //наличие и тип модификатора (CNTRL, SHIFT, ALT)
+    unsigned int typeModifierQuit = Qt::ControlModifier; //наличие и тип модификатора (CNTRL, SHIFT, ALT)
+    unsigned int typeModifierNewFile = Qt::ControlModifier; //наличие и тип модификатора (CNTRL, SHIFT, ALT)
+
+    bool checkKeys(unsigned int typeModifier, int key); //определяет, используется ли уже данная клавиша (сочетание клавиш)
+
+    /*Для проверки нажатия горячих клавиш*/
+    static void openIsPressed(MainWindow*, QKeyEvent *);
+    static void saveIsPressed(MainWindow*, QKeyEvent *);
+    static void newKeyIsPressed(MainWindow*, QKeyEvent *);
+    static void quitIsPressed(MainWindow*, QKeyEvent *);
+    using funcMap = std::map <int, void (*)(MainWindow *, QKeyEvent *)>;
+    funcMap keyPressedFunc;
+
+    /*Для обновления комбинации горячих клавиш*/
+    void updateCombination(int &key, unsigned int &typeModifier, void (*functPressed)(MainWindow *, QKeyEvent *), QKeyEvent * event);
+
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
-private:
-    Ui::MainWindow *ui;
+private slots:
+    void on_actWhite_triggered();
+
+    void on_actDark_triggered();
 };
 
 #endif // MAINWINDOW_H
